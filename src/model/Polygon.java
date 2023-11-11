ceb3fb74d4ce08ea7e6c65837da657ae136fc1cd
Nadezhda <nadyagr@bk.ru>
2023-11-11 17:35:42 +0100

    Add Polygon model

--- src/model/Polygon.java
+++ src/model/Polygon.java
@@ -1,7 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Polygon {
+    private ArrayList<Point> points;
 
-	//TODO
+    public Polygon() {
+        this.points = new ArrayList<>();
+    }
 
+    public void addPoint(Point p) {
+        this.points.add(p);
+    }
+
+    public int size() {
+        return this.points.size();
+    }
+
+    public Point getPoint(int index) {
+        return this.points.get(index);
+    }
 }
+
+
