de288f149f079e1cfc56f1289130e48838d86a45
Nadezhda <nadyagr@bk.ru>
2023-11-11 17:33:32 +0100

    Add PolygonRasterizer

--- src/rasterize/PolygonRasterizer.java
+++ src/rasterize/PolygonRasterizer.java
@@ -7,7 +7,22 @@ import model.Polygon;
 import java.util.List;
 
 public class PolygonRasterizer {
+    private LineRasterizer lineRasterizer;
+    public PolygonRasterizer(LineRasterizer lineRasterizer) {
+        this.lineRasterizer = lineRasterizer;
+    }
+    public void rasterize(Polygon polygon) {
+        if (polygon.size() < 3)
+            return;
 
-    //TODO
+        for (int i = 0; i < polygon.size(); i++) {
+            int indexA = i;
+            int indexB = (i + 1) % polygon.size();
 
+            Point pA = polygon.getPoint(indexA);
+            Point pB = polygon.getPoint(indexB);
+
+            lineRasterizer.rasterize(new Line(pA, pB, 0xf658b8));
+        }
+    }
 }
