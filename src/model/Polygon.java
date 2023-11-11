be325ebe49e7d3eb4209ab1c358c600a687ecfbe
Nadezhda <nadyagr@bk.ru>
2023-11-11 17:41:19 +0100

    fix

--- src/model/Polygon.java
+++ src/model/Polygon.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 
 public class Polygon {
     private ArrayList<Point> points;
-
     public Polygon() {
         this.points = new ArrayList<>();
     }
-
     public void addPoint(Point p) {
         this.points.add(p);
     }
-
     public int size() {
         return this.points.size();
     }
-
     public Point getPoint(int index) {
         return this.points.get(index);
     }
