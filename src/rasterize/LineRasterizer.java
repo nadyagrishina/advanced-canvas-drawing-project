55da7989c813e2519db629ff541a744ace2ba062
Nadezhda <nadyagr@bk.ru>
2023-11-12 17:43:34 +0100

    Fix

--- src/rasterize/LineRasterizer.java
+++ src/rasterize/LineRasterizer.java
@@ -11,19 +11,24 @@ public abstract class LineRasterizer {
     public LineRasterizer(Raster raster){
         this.raster = raster;
     }
+
     public void setColor(Color color) {
         this.color = color;
     }
+
     public void setColor(int color) {
         this.color = new Color(color);
     }
-    public void rasterize(Line line) {
-        // TODO: co s barvou?
-        drawLine(line.getX1(), line.getY1(), line.getX2(), line.getY2());
+
+    public void rasterize(Line line, Color color) {
+        drawLine(line.getX1(), line.getY1(), line.getX2(), line.getY2(), color);
     }
+
     public void rasterize(int x1, int y1, int x2, int y2, Color color) {
-        drawLine(x1, y1, x2, y2);
+        drawLine(x1, y1, x2, y2, color);
     }
-    protected void drawLine(int x1, int y1, int x2, int y2) {
+
+    protected void drawLine(int x1, int y1, int x2, int y2, Color color) {
+
     }
 }
